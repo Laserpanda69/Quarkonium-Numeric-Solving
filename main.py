15c39fb1ba324e9121c27c7105afdb40acf9051a
Josh <joshread7@hotmail.co.uk>
2025-09-21 16:11:20 +0100

    moved mass solving to a seperate file

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ from potentialModels import cornell_potential, bhanot_rudaz_potential
 
 from linestyles import LineStyle, line_styles_dict, line_styles_list
 
+from massSolver import calculate_meson_masses
+
 # https://pdg.lbl.gov/
 
 GROUND_STATE = '1S'
@@ -42,6 +44,7 @@ initial_calibration_var_bottomonium_cornell = 0.006
 # User set variables
 N = 3
 F = 30
+REDNER = False
 initial_calibration_variable = initial_calibration_var_charmonium_cornell
 meson = charmonium_1S
 wavefunction = bhanot_rudaz_wave_function
@@ -50,7 +53,8 @@ potential_model = bhanot_rudaz_potential
 
 # Grey line at 0
 
-plt.plot(r_space, [0]*len(r_space), linestyle = line_styles_dict[LineStyle.LOOSELY_DASHED], color = "grey")
+if REDNER:
+    plt.plot(r_space, [0]*len(r_space), linestyle = line_styles_dict[LineStyle.LOOSELY_DASHED], color = "grey") 
 
 ###########################################
 ##### POTENTIAL PARAMETER CALIBRATION #####
@@ -68,6 +72,11 @@ print(f"calibration got {calibrated_variable}")
 pdf, u, v = sol
 plt.plot(r_space, pdf, color = 'magenta', linestyle = line_styles_dict[LineStyle.LOOSELY_DASHDOTTED], linewidth = 4, label = "calibration")
 
+binding_energies, masses = calculate_meson_masses(meson, r_space, wavefunction, N, calibrated_variable, render=REDNER)
+
+for n in range(N+1):
+    for l in range(n):
+        print(f"M_{n}{l} = {masses[n][l]}")
 
 ##################################
 ##### POTENTIAL PDF PLOTTING #####
@@ -84,75 +93,9 @@ plt.plot(r_space, pdf, color = 'magenta', linestyle = line_styles_dict[LineStyle
 
 # Numerical solving of exciterd states
 
-last_energy_value = meson.binding_energy - 0.1
-offset = 0.01
-
-print("Solving")
-color_cycle = plt.rcParams["axes.prop_cycle"].by_key()["color"]
-for n in range(1, N+1):
-    pdf_maxima = 0
-    min_tp_peak = 100
-    for l in range(0, n):
-        E, sol, points_of_interest = numericalSolvers.solve_for_energy(
-            U0, r_space, wavefunction, n,
-            potential_arguments=(l, calibrated_variable, meson.reduced_mass),
-            epsilon_lower = last_energy_value + offset, 
-            flight = F
-        )
-        pdf, u, v = sol
-        nodes, turning_points = points_of_interest
-
-        color = color_cycle[n-1]
-        linestyle = line_styles_list[l]
-        plt.plot(r_space, pdf, color = color, linestyle = linestyle, label = f"{n}{l}")
-
-        # pdf_maximak_line = []
-        
-        if n == 1:
-            print(f"numerically solved meson ground state energy = {meson.binding_energy}")
-            print(f"reference meson energy = {meson.binding_energy}")
-            print(f"Delta = {meson.binding_energy - E}")
-            print("")
-            
-        pdf_turning_point_peaks = wfns.find_pdf_peaks(r_space, pdf, turning_points['positions'])
-
-
-        local_maxima = max(pdf_turning_point_peaks)
-        pdf_maxima =  local_maxima if local_maxima > pdf_maxima else pdf_maxima
-        pdf_maxima_line = [pdf_maxima]*len(r_space)
-        
-        plt.plot(r_space, pdf_maxima_line, 
-            linestyle = line_styles_dict[LineStyle.LOOSELY_DOTTED], color = color, alpha = 0.4)
-
-
-
-        tollerance = 0.01
-        for i, amp in enumerate(pdf_turning_point_peaks):
-            if amp < tollerance:
-                pdf_turning_point_peaks.pop(i)
-                turning_points['positions'].pop(i)
-
-        plt.scatter(turning_points['positions'], pdf_turning_point_peaks, color = color, linewidths= .5)
-        
-        local_lowest_peak = min(pdf_turning_point_peaks)
-        min_tp_peak = local_lowest_peak if local_lowest_peak < min_tp_peak else min_tp_peak
-        min_tp_peak_line = [min_tp_peak]*len(r_space)
-        plt.plot(r_space, min_tp_peak_line, 
-            linestyle = line_styles_dict[LineStyle.LOOSELY_DOTTED], color = color, alpha = 0.4)
-
-            
-
-
-            
-        print(f"E_{n}{l} = {E}")
-        # print(f"M_{n}{l} = {E+2*charm_quark_mass}")
-        last_energy_value = E
-    plt.fill_between(r_space, min_tp_peak_line, pdf_maxima_line, where=(min_tp_peak < pdf_maxima_line), color=color, alpha=0.1)
-
-
-
-plt.legend()
-plt.show()
+if REDNER:
+    plt.legend()
+    plt.show()
 
 
 
---
+++ massSolver.py
@@ -0,0 +1,108 @@
+import numpy as np  
+import matplotlib.pyplot as plt
+import scipy.integrate 
+
+import wavefuncitons as wfns
+
+from enum import Enum
+from data import ParticleName, particle_masses, ColorCharge
+
+import numericalSolvers as Solvers
+
+from Particles.Fermions.Quarks import *
+from Particles.Hadrons.Hadron import Hadron
+from Particles.Hadrons import Mesons 
+
+import numericalSolvers
+
+from wavefuncitons import corenell_wave_function, bhanot_rudaz_wave_function
+from potentialModels import cornell_potential, bhanot_rudaz_potential
+
+from linestyles import LineStyle, line_styles_dict, line_styles_list
+
+# https://pdg.lbl.gov/
+
+GROUND_STATE = '1S'
+REFERENCE = 'reference'
+STAIRCASE = 'staircase'
+U0 = [0,1]
+
+# Grey line at 0
+
+def calculate_meson_masses(meson: Mesons.Meson,r_space,wavefunction,n_states_count,
+    calibrated_variable,flights=30,render=True,
+) -> tuple[dict]:
+
+    if render:
+        plt.plot(r_space, [0]*len(r_space), linestyle = line_styles_dict[LineStyle.LOOSELY_DASHED], color = "grey")
+
+    ##############################
+    ##### MESON PDF PLOTTING #####
+    ##############################
+
+    # Numerical solving of exciterd states
+
+    last_energy_value = meson.binding_energy - 0.1
+    offset = 0.01
+
+    print("Solving")
+    color_cycle = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+
+    binding_energies = [[]]
+    masses = [[]]
+
+    for n in range(1, n_states_count+1):
+        binding_energies.append([])
+        masses.append([])
+        
+        pdf_maxima = 0
+        min_tp_peak = 100
+        
+        for l in range(0, n):
+            binding_energy, sol, points_of_interest = numericalSolvers.solve_for_energy(
+                U0, r_space, wavefunction, n,
+                potential_arguments=(l, calibrated_variable, meson.reduced_mass),
+                epsilon_lower = last_energy_value + offset, 
+                flight = flights
+            )
+            pdf, u, v = sol
+
+            # print(f"E_{n}{l} = {E}")
+            # print(f"M_{n}{l} = {E+2*charm_quark_mass}")
+            last_energy_value = binding_energy
+
+            binding_energies[n].append(binding_energy)
+            quark_mass = sum(q.mass for q in meson.quarks)
+            masses[n].append(binding_energy + quark_mass)
+
+            if render:
+                nodes, turning_points = points_of_interest
+                color = color_cycle[n-1]
+                linestyle = line_styles_list[l]
+
+                pdf_turning_point_peaks = wfns.find_pdf_peaks(r_space, pdf, turning_points['positions'])
+
+                local_maxima = max(pdf_turning_point_peaks)
+                pdf_maxima =  local_maxima if local_maxima > pdf_maxima else pdf_maxima
+                pdf_maxima_line = [pdf_maxima]*len(r_space)
+
+                tollerance = 0.01
+                for i, amp in enumerate(pdf_turning_point_peaks):
+                    if amp < tollerance:
+                        pdf_turning_point_peaks.pop(i)
+                        turning_points['positions'].pop(i)
+
+                local_lowest_peak = min(pdf_turning_point_peaks)
+                min_tp_peak = local_lowest_peak if local_lowest_peak < min_tp_peak else min_tp_peak
+                min_tp_peak_line = [min_tp_peak]*len(r_space)
+
+                plt.scatter(turning_points['positions'], pdf_turning_point_peaks, color = color, linewidths= .5)
+                plt.plot(r_space, pdf, color=color, linestyle=linestyle, label=f"{n}{l}")
+                plt.plot(r_space,pdf_maxima_line,linestyle=line_styles_dict[LineStyle.LOOSELY_DOTTED],color=color,alpha=0.4,)
+                plt.plot(r_space, min_tp_peak_line, linestyle = line_styles_dict[LineStyle.LOOSELY_DOTTED], color = color, alpha = 0.4)
+
+        if render:
+            plt.fill_between(r_space, min_tp_peak_line, pdf_maxima_line, where=(min_tp_peak < pdf_maxima_line), color=color, alpha=0.1)
+            plt.legend()
+
+    return binding_energies, masses
